534eb08d899a09c128ef8d58ec99e73d8e0567d3
cloudglides <authqtx@gmail.com>
2025-02-12 13:04:00 +0530

    better env

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,14 @@
 import Config
 
+
+if config_env() in [:dev, :test] do
+  for path <- [".env.exs", ".env.#{config_env()}.exs"] do
+    path = Path.join(__DIR__, "..") |> Path.join("config") |> Path.join(path) |> Path.expand()
+    if File.exists?(path), do: import_config(path)
+  end
+end
+
+
 config :nostrum,
   token: System.get_env("TOKEN"), 
   gateway_intents: :all,
