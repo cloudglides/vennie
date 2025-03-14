0360bfaf4fcdf56516787527d5f22a0a93071dcd
cloudglides <authqtx@gmail.com>
2025-03-14 20:09:55 +0530

    WORKING BUTTONS LETS GO

--- lib/commands/miscellaneous.ex
+++ lib/commands/miscellaneous.ex
@@ -32,7 +32,6 @@ defmodule Commands.Miscellaneous do
   end
 
   def help(%{msg: msg, args: _args}) do
-    # Create the button
     buttons = [
       %Nostrum.Struct.Component{
         type: :button,
@@ -42,46 +41,53 @@ defmodule Commands.Miscellaneous do
       }
     ]
 
-    # Create the embed
-    embed =
+    embed_1 =
       %Nostrum.Struct.Embed{}
       |> Nostrum.Struct.Embed.put_title("Help Command")
       |> Nostrum.Struct.Embed.put_description(
         "List of available commands for this bot.\n\nPrefix: v or V"
       )
-      |> Nostrum.Struct.Embed.put_color(431_948)
+      |> Nostrum.Struct.Embed.put_color(0x808080)
       |> Nostrum.Struct.Embed.put_field("help/h", "Shows this help command.")
-      |> Nostrum.Struct.Embed.put_field("howdumb/hd", "Gives a percentage of how dumb you are.")
-      |> Nostrum.Struct.Embed.put_field("rank/r", "Displays your rank details.")
-      |> Nostrum.Struct.Embed.put_field("websocket/ws", "Shows the current WebSocket details.")
+
+    embed_2 =
+      %Nostrum.Struct.Embed{}
+      |> Nostrum.Struct.Embed.put_title("Mod Commands")
+      |> Nostrum.Struct.Embed.put_color(431_948)
       |> Nostrum.Struct.Embed.put_field("mute/m", "Mutes a user.")
-      |> Nostrum.Struct.Embed.put_field("whyban/wb", "States the reason for a user's ban.")
       |> Nostrum.Struct.Embed.put_field("unban/ub", "Unbans a user.")
       |> Nostrum.Struct.Embed.put_field("lock/l", "Locks the current thread.")
       |> Nostrum.Struct.Embed.put_field("kick/k", "Kicks a user from the server.")
       |> Nostrum.Struct.Embed.put_field("ban/b", "Bans a user.")
+      |> Nostrum.Struct.Embed.put_field("websocket/ws", "Shows the current WebSocket details.")
 
-    # Create the button component
+    embed_3 =
+      %Nostrum.Struct.Embed{}
+      |> Nostrum.Struct.Embed.put_title("Misc Commands")
+      |> Nostrum.Struct.Embed.put_color(431_948)
+      |> Nostrum.Struct.Embed.put_field("howdumb/hd", "Gives a percentage of how dumb you are.")
+      |> Nostrum.Struct.Embed.put_field("rank/r", "Displays your rank details.")
+      |> Nostrum.Struct.Embed.put_field("whyban/wb", "States the reason for a user's ban.")
+
+    embed_list = [embed_1, embed_2, embed_3]
+
     button =
-      Nostrum.Struct.Component.Button.interaction_button("refresh", "trackmania_refresh",
-        emoji: %Nostrum.Struct.Emoji{name: "refresh", id: 1_200_130_727_187_054_613}
-      )
+      Nostrum.Struct.Component.Button.interaction_button("Right", "right", style: 2)
 
-    components = Nostrum.Struct.Component.ActionRow.action_row(components: [button])
+    button2 =
+      Nostrum.Struct.Component.Button.interaction_button("Left", "left", style: 2)
 
-    # Log the message details
+    components = Nostrum.Struct.Component.ActionRow.action_row(components: [button2, button])
+
     Logger.debug("User ID: #{msg.author.id}")
     Logger.debug("Message ID: #{msg.id}")
 
-    # Create the bot's message with the embed and components (buttons)
     {:ok, sent_message} =
-      Api.create_message(msg.channel_id, embed: embed, components: [components])
+      Api.create_message(msg.channel_id, embed: embed_1, components: [components])
 
-    # Store the message ID of the message the bot sent
     Logger.debug("Bot Sent Message ID: #{sent_message.id}")
-    HandleOp.store_message(msg.author.id, sent_message.id)
+    HandleOp.store_message(msg.author.id, sent_message.id, "embed_1")
 
-    # Optionally, if you want to retrieve the user ID from a message and store it
     userid = HandleOp.get_userid(msg.id)
   end
 end
--- lib/utils/handle_op.ex
+++ lib/utils/handle_op.ex
@@ -6,24 +6,33 @@ defmodule HandleOp do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
-  def store_message(userid, messageid) do
-    GenServer.cast(__MODULE__, {:store_message, userid, messageid})
+  def store_message(userid, messageid, embed) do
+    GenServer.cast(__MODULE__, {:store_message, userid, messageid, embed})
   end
 
   def get_userid(messageid) do
     GenServer.call(__MODULE__, {:get_userid, messageid})
   end
 
+  def get_embed(messageid) do
+    GenServer.call(__MODULE__, {:get_embed, messageid})
+  end
+
   def init(_) do
     {:ok, %{}}
   end
 
-  def handle_cast({:store_message, userid, messageid}, state) do
-    Logger.debug("genserver #{messageid}")
-    Logger.debug("genserver #{userid}")
-    new_state = Map.put(state, messageid, %{userid: userid, timestamp: :os.system_time(:seconds)})
-    Process.send_after(self(), {:expire_message, messageid}, 60_000)
+  def handle_cast({:store_message, userid, messageid, embed}, state) do
+    Logger.debug("genserver storing message #{messageid} with embed #{embed}")
 
+    new_state =
+      Map.put(state, messageid, %{
+        userid: userid,
+        embed: embed,
+        timestamp: :os.system_time(:seconds)
+      })
+
+    Process.send_after(self(), {:expire_message, messageid}, 60_000)
     {:noreply, new_state}
   end
 
@@ -35,6 +44,21 @@ defmodule HandleOp do
       %{userid: userid, timestamp: timestamp} ->
         if :os.system_time(:seconds) - timestamp < 60 do
           {:reply, userid, state}
+        else
+          new_state = Map.delete(state, messageid)
+          {:reply, nil, new_state}
+        end
+    end
+  end
+
+  def handle_call({:get_embed, messageid}, _from, state) do
+    case Map.get(state, messageid) do
+      nil ->
+        {:reply, nil, state}
+
+      %{embed: embed, timestamp: timestamp} ->
+        if :os.system_time(:seconds) - timestamp < 60 do
+          {:reply, embed, state}
         else
           new_state = Map.delete(state, messageid)
           {:reply, nil, new_state}
